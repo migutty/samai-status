174f7b2f198a28084b1590dbae5b210a8bca92ee
Miguel <migutty@gmail.com>
2026-05-16 01:22:42 -0500

    Fixed Incident import

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.database.session import engine, Base
 
-from app.models.incident import incident
+from app.models.incident import Incident
 
 
 from app.routes.incident import router as incident_router
